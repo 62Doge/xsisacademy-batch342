8e0109705b0d08789390e5acc6e082384c9e915d
Raynaldi Faizal Biqri <hellobiqri@gmail.com>
2024-10-31 15:19:59 +0700

    add: Several packages & LocationLevel done

--- backend/src/main/java/com/_a/backend/entities/LocationLevel.java
+++ backend/src/main/java/com/_a/backend/entities/LocationLevel.java
@@ -1,4 +1,30 @@
 package com._a.backend.entities;
 
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "m_location_level")
+@AllArgsConstructor
+@NoArgsConstructor
 public class LocationLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "abbreviation", length = 50)
+    private String abbreviation;
+
+//    This is for m_location relation
+//    @JsonManagedReference
+//    private List<Location> locations;
 }
