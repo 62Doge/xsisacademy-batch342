151c10fd9cee0cfa951227e04e85d78e6ca08adb
Raynaldi Faizal Biqri <hellobiqri@gmail.com>
2024-11-01 08:53:50 +0700

    Commit added: -WebConfig -LocationLevelController -LocationLevelDTO -Services interface -LocationLevelServiceImpl

---
+++ backend/src/main/java/com/_a/backend/dtos/responses/LocationLevelResponseDTO.java
@@ -0,0 +1,10 @@
+package com._a.backend.dtos.responses;
+
+import lombok.Data;
+
+@Data
+public class LocationLevelResponseDTO {
+    private Long id;
+    private String name;
+    private String abbreviation;
+}
---
+++ backend/src/main/java/com/_a/backend/services/impl/LocationLevelServiceImpl.java
@@ -0,0 +1,54 @@
+package com._a.backend.services.impl;
+
+import com._a.backend.dtos.requests.LocationLevelRequestDTO;
+import com._a.backend.dtos.responses.LocationLevelResponseDTO;
+import com._a.backend.entities.LocationLevel;
+import com._a.backend.repositories.LocationLevelRepository;
+import com._a.backend.services.Services;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LocationLevelServiceImpl implements Services<LocationLevelRequestDTO, LocationLevelResponseDTO> {
+    @Autowired
+    private LocationLevelRepository locationLevelRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public List<LocationLevelResponseDTO> findAll() {
+        List<LocationLevel> locationLevels = locationLevelRepository.findAll();
+        List<LocationLevelResponseDTO> locationLevelResponseDTOs = locationLevels.stream().map(
+                locationLevel -> modelMapper.map(locationLevel, LocationLevelResponseDTO.class)).toList();
+        return locationLevelResponseDTOs;
+    }
+
+    @Override
+    public Optional<LocationLevelResponseDTO> findById(Long id) {
+        Optional<LocationLevel> locationLevel = locationLevelRepository.findById(id);
+        if (locationLevel.isPresent()) {
+            Optional<LocationLevelResponseDTO> locationLevelResponseDTO = locationLevel.map(
+                    locationLevelOptional -> modelMapper.map(locationLevelOptional, LocationLevelResponseDTO.class));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public LocationLevelResponseDTO save(LocationLevelRequestDTO locationLevelRequestDTO) {
+        LocationLevel locationLevel = locationLevelRepository.save(modelMapper.map(locationLevelRequestDTO, LocationLevel.class));
+        LocationLevelResponseDTO locationLevelResponseDTO = modelMapper.map(locationLevel, LocationLevelResponseDTO.class);
+        return locationLevelResponseDTO;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        locationLevelRepository.deleteById(id);
+    }
+}
