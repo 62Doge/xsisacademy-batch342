e72f1579404fc8bb4f2bb88943c154ea97e5f30a
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-11-01 15:18:50 +0700

    add: Doctor entity

---
+++ backend/src/main/java/com/_a/backend/entities/Specialization.java
@@ -0,0 +1,36 @@
+package com._a.backend.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@Table(name = "m_specialization")
+public class Specialization extends BaseEntity{
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "name", length = 50)
+  private String name;
+
+  @OneToMany(mappedBy = "specialization")
+  @JsonBackReference
+  private List<CurrentDoctorSpecialization> currentDoctorSpecializations;
+}
