0a8677b65c8c38e1a93c9ac2ec8231e884bc1587
mutiawsr <mutiawsr@gmail.com>
2024-11-01 07:51:10 +0700

    add: BaseEntity class

---
+++ backend/src/main/java/com/_a/backend/entities/BaseEntity.java
@@ -0,0 +1,44 @@
+package com._a.backend.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", updatable = false)
+    private LocalDateTime createdOn;
+
+    @Column(name = "modified_by", updatable = true, nullable = true)
+    private Long modifiedBy;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modefied_on", updatable = true, nullable = true)
+    private LocalDateTime modifiedOn;
+
+    @Column(name = "deleted_by", nullable = true)
+    private Long deletedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "deleted_on", nullable = true)
+    private LocalDateTime deletedOn;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+}
