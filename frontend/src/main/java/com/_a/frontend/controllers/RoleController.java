57cd896df462d31e03758d190f89c058cb493e62
Sch39 <sukronraezpactoer@gmail.com>
2024-11-08 14:49:11 +0700

    feat: slicing fe role

--- frontend/src/main/java/com/_a/frontend/controllers/RoleController.java
+++ frontend/src/main/java/com/_a/frontend/controllers/RoleController.java
@@ -3,33 +3,29 @@ package com._a.frontend.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/role")
 public class RoleController {
 
-    @GetMapping("")
+    @GetMapping({ "", "/" })
     public String index() {
-        return "pages/role-access/index";
+        return "pages/role/index";
     }
 
-    @GetMapping("/addForm")
-    public ModelAndView form() {
-        ModelAndView view = new ModelAndView("pages/role-access/addForm");
-        return view;
+    @GetMapping({ "/addForm", "/addForm/" })
+    public String form() {
+        return "pages/role/addForm";
     }
 
-    @GetMapping("/editForm")
-    public ModelAndView edit() {
-        ModelAndView view = new ModelAndView("pages/role-access/editForm");
-        return view;
+    @GetMapping({ "/editForm", "/editForm/" })
+    public String edit() {
+        return "pages/role/editForm";
     }
 
-    @GetMapping("/deleteModal")
-    public ModelAndView delete() {
-        ModelAndView view = new ModelAndView("pages/role-access/deleteModal");
-        return view;
+    @GetMapping({ "/deleteModal", "/deleteModal/" })
+    public String delete() {
+        return "pages/role/deleteModal";
     }
 
 }
