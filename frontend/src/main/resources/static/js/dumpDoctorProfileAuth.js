8d4ca32f49bcf87b7718b3447c7ce24474551396
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-11-13 23:30:16 +0700

    feat: add login as doctor

---
+++ frontend/src/main/resources/static/js/dumpDoctorProfileAuth.js
@@ -0,0 +1,30 @@
+let IS_USER_LOGGED = false;
+let USER_LOGGED_ID = -1;
+let ADMIN_LOGGED_ID = 0;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Mengecek jika pengguna tidak masuk atau bukan admin
+  if (USER_LOGGED_ID !== -1 && USER_LOGGED_ID !== null) {
+    IS_USER_LOGGED = true;
+    $("#sidebarToggle").removeClass("d-none"); // Menampilkan tombol sidebar
+    // Menampilkan sementara sidebar dan navbar
+    $('#layout-menu').removeClass('d-none');
+    $('#layout-navbar').removeClass('d-none');
+
+    // mengaktifkan semua elemen dalam "col-md-8"
+    $(".col-md-8 *").prop("disabled", false).css("pointer-events", "");
+  } else {
+    IS_USER_LOGGED = false;
+    $("#sidebarToggle").addClass("d-none"); // Menyembunyikan tombol sidebar
+
+    // Menghilangkan sementara sidebar dan navbar
+    $('#layout-menu').addClass('d-none');
+    $('#layout-navbar').addClass('d-none');
+
+    // Menonaktifkan semua elemen dalam "col-md-8"
+    $(".col-md-8 *").prop("disabled", true).css("pointer-events", "none");
+    
+  }
+
+});
+
